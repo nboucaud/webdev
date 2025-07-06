6840e6a644858440f01ed6613c309d3667dc6592
jem3ii <ayoubjemai@takiacademyteam.com>
2025-07-06 14:06:29 +0100

    fix bug

--- apps/web/client/src/components/ui/dunes.tsx
+++ apps/web/client/src/components/ui/dunes.tsx
@@ -3,16 +3,16 @@ import Image from 'next/image';
 export function Dunes() {
     return (
         <div className="hidden w-full lg:block md:block m-6">
-            <Image
+            <img
                 className="w-full h-full object-cover rounded-xl hidden dark:flex"
-                src={'/assets/dunes-login-dark.png'}
+                src={'https://webdev.infogito.com/assets/dunes-login-dark.png'}
                 alt="Onlook dunes dark"
                 width={1000}
                 height={1000}
             />
-            <Image
+            <img
                 className="w-full h-full object-cover rounded-xl flex dark:hidden"
-                src={'/assets/dunes-login-light.png'}
+                src={'https://webdev.infogito.com/assets/dunes-login-light.png'}
                 alt="Onlook dunes light"
                 width={1000}
                 height={1000}
